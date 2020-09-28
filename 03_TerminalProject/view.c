0b262fc28a08323947a858d44cee9c440f46dfb9
Nikolai Oplachko <magnickolas@gmail.com>
2020-09-28 20:13:54 +0300

    add vertical scroll support

--- 03_TerminalProject/view.c
+++ 03_TerminalProject/view.c
@@ -1,10 +1,12 @@
-#include <ncurses.h>
+#include <assert.h>
 #include <locale.h>
+#include <ncurses.h>
+#include <string.h>
 #include <sys/stat.h>
 
 #define KEY_ESC 27
 
-ssize_t get_line_length(FILE* fp) {
+ssize_t get_line_chars_num(FILE* fp) {
     ssize_t res = 0;
     int c;
     while ((c = fgetc(fp)) != EOF && c != '\n') {
@@ -12,6 +14,23 @@ ssize_t get_line_length(FILE* fp) {
     }
     if (c == EOF && res == 0) {
         return -1;
+    }
+    res += (c == '\n');
+    return res;
+}
+
+ssize_t get_line_chars_num_backwards(FILE* fp) {
+    ssize_t res = 0;
+    int c;
+    if (ftell(fp) == 0) {
+        return 2 - (fgetc(fp) == '\n');
+    }
+    while (ftell(fp) > 0 && (c = fgetc(fp)) != '\n') {
+        res++;
+        fseek(fp, -2, SEEK_CUR);
+    }
+    if (ftell(fp) == 0) {
+        res += 2;
     }
     res += (c == '\n');
     return res;
@@ -53,10 +72,10 @@ int main(int argc, char** argv) {
     block_lines_start_poses[0] = 0;
     ssize_t block_lines_start_offset = 0;
     ssize_t block_lines_offset = 0;
-    int lines_num = 0;
+    ssize_t lines_num = 0;
 
-    for (int i = 1; i <= max_lines_num; i++) {
-        ssize_t len = get_line_length(fp);
+    for (ssize_t i = 1; i <= max_lines_num; i++) {
+        ssize_t len = get_line_chars_num(fp);
         if (len == -1) {
             break;
         }
@@ -69,38 +88,69 @@ int main(int argc, char** argv) {
 
     while (!quit) {
         clear();
+        ssize_t max_block_line_length = 0;
         // Status line
         printw("%s--%ld:%ld", fname, block_lines_offset, block_lines_start_offset);
         hline('-', width);
         move(1, 0);
-        //
-        for (int i = 0; i < lines_num; i++) {
+        // Output file content block
+        for (ssize_t i = 0; i < lines_num; i++) {
             ssize_t pos_to_seek = block_lines_start_poses[i] + block_lines_start_offset;
             if (pos_to_seek >= block_lines_start_poses[i+1]) {
                 pos_to_seek = block_lines_start_poses[i+1] - 1;
             }
             fseek(fp, pos_to_seek, SEEK_SET);
             fgets(line_buf, width, fp);
+            ssize_t len = strlen(line_buf);
+            if (len > max_block_line_length) {
+                max_block_line_length = len;
+            }
             printw("%s", line_buf);
         }
         refresh();
+
         int c = getch();
         if (c == KEY_UP || c == 'k') {
             // Forward shift by 1 line, get the top
             if (block_lines_offset > 0) {
                 block_lines_offset--;
+                if (lines_num < max_lines_num) {
+                    lines_num++;
+                }
+                for (ssize_t i = lines_num; i > 0; i--) {
+                    block_lines_start_poses[i] = block_lines_start_poses[i-1];
+                }
+                assert(block_lines_start_poses[0] >= 1);
+                ssize_t backward_offset = 2 - (block_lines_start_poses[0] == 1);
+                fseek(fp, block_lines_start_poses[0] - backward_offset, SEEK_SET);
+                ssize_t len = get_line_chars_num_backwards(fp);
+                block_lines_start_poses[0] -= len;
             }
-            // TODO
         } else if (c == KEY_DOWN || c == 'j' || c == ' ') {
             // Backward shift by 1 line, get the bottom
-            block_lines_offset++;
-            // TODO
+            if (lines_num > 0) {
+                block_lines_offset++;
+                for (ssize_t i = 0; i < lines_num; i++) {
+                    block_lines_start_poses[i] = block_lines_start_poses[i+1];
+                }
+                fseek(fp, block_lines_start_poses[lines_num], SEEK_SET);
+                ssize_t len = get_line_chars_num(fp);
+                if (len != -1) {
+                    block_lines_start_poses[lines_num] += len;
+                } else {
+                    if (lines_num > 0) {
+                        lines_num--;
+                    }
+                }
+            }
         } else if (c == KEY_LEFT || c == 'h') { // Decrease line beginning offset by 1
             if (block_lines_start_offset > 0) {
                 block_lines_start_offset--;
             }
         } else if (c == KEY_RIGHT || c == 'l') { // Increase line beginning offset by 1
-            block_lines_start_offset++;
+            if (max_block_line_length > 1) {
+                block_lines_start_offset++;
+            }
         } else if (c == 'q' || c == KEY_ESC) {
             quit = true;
         }
