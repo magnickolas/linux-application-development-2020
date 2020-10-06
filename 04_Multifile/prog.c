4f02688c19576eedfb51389c31fdbfb5432a4d03
Nikolai Oplachko <magnickolas@gmail.com>
2020-10-06 23:46:24 +0300

    add format file

--- 04_Multifile/prog.c
+++ 04_Multifile/prog.c
@@ -1,7 +1,7 @@
 #include "outlib.h"
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     int i;
     if ((Count = argc) > 1) {
         output("<INIT>");
